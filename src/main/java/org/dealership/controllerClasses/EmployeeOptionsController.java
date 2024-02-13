34564557b8e075fa15fd1eb486e71d95e509f2cb
Chandler Ward <chandlerward18@gmail.com>
2024-02-13 14:43:38 -0500

    Updated Controller and FXML files
    New commit that fixes the controller classes that were missing some parts and the FXML files that had some issues with controller classes.
    Chandler

--- src/main/java/org/dealership/controllerClasses/EmployeeOptionsController.java
+++ src/main/java/org/dealership/controllerClasses/EmployeeOptionsController.java
@@ -15,49 +15,55 @@
 
  package org.dealership.controllerClasses;
 
- import javafx.event.ActionEvent;
+ import java.io.IOException;
+
+import org.dealership.driverClass;
+
+import javafx.event.ActionEvent;
  import javafx.fxml.FXML;
  
  public class EmployeeOptionsController {
+
+    driverClass driver = new driverClass();
  
-      @FXML
-     void BuyVehicle(ActionEvent event) {
- 
-     }
- 
-     @FXML
-     void CreateInvoice(ActionEvent event) {
- 
-     }
- 
-     @FXML
-     void OrderVehicle(ActionEvent event) {
- 
-     }
- 
-     @FXML
-     void RecCustomerInfo(ActionEvent event) {
- 
-     }
- 
-     @FXML
-     void SearchVehicleInfo(ActionEvent event) {
- 
-     }
- 
-     @FXML
-     void SignOut(ActionEvent event) {
- 
-     }
- 
-     @FXML
-     void TradeInVehicle(ActionEvent event) {
- 
-     }
- 
-     @FXML
-     void WeeklyDiscounts(ActionEvent event) {
- 
-     }
+    @FXML
+    void BuyVehicle(ActionEvent event) throws IOException {
+        driver.changeScene("CustomerBuyingVehiclePanel.fxml");
+    }
+
+    @FXML
+    void CreateInvoice(ActionEvent event) throws IOException {
+        driver.changeScene("CreateInvoicePanel.fxml");
+    }
+
+    @FXML
+    void OrderVehicle(ActionEvent event) throws IOException {
+        driver.changeScene("OrderVehiclePage.fxml");
+    }
+
+    @FXML
+    void RecCustomerInfo(ActionEvent event) throws IOException {
+        driver.changeScene("RecordCustomerInfoPanel.fxml");
+    }
+
+    @FXML
+    void SearchVehicleInfo(ActionEvent event) throws IOException {
+        driver.changeScene("SearchVehicleInformationPanel.fxml");
+    }
+
+    @FXML
+    void SignOut(ActionEvent event) throws IOException {
+        driver.changeScene("LoginScreenPanel.fxml");
+    }
+
+    @FXML
+    void TradeInVehicle(ActionEvent event) throws IOException {
+        driver.changeScene("VehicleTradeInInformationPanel.fxml");
+    }
+
+    @FXML
+    void WeeklyDiscounts(ActionEvent event) throws IOException {
+        driver.changeScene("WeeklyDealsPanel.fxml");
+    }
      
  }

447cffc6f9a9450ece23a12024572f7fbf332b3c
Chandler Ward <chandlerward18@gmail.com>
2024-03-19 14:49:39 -0400

    Invoice Functionality (not completed)
    This is a commit to add the functionality for the invoice class and corresponding classes.
    
    The code is not complete and needs to be completed so the page doesn't function fully, so do not try to use, if you do you will get errors.

--- src/main/java/org/dealership/Entities/invoice.java
+++ src/main/java/org/dealership/Entities/invoice.java
@@ -1,5 +1,5 @@
 /*
- * Author:       Chandler Ward
+ * Author:       Chandler Ward, Zhania Douglas
  * Written:      2 / 6 / 2024
  * Last Updated: 2 / 6 / 2024
  * 
@@ -12,6 +12,56 @@
 
 package org.dealership.Entities;
 
-public class Invoice {
+public class Invoice{
+
+    // Data Fields for each attribute
+    String custAddress = "No Address Specified";
+    String fees = "No Address Specified";
+    String custID = "No Address Specified";
+    String discountTotal = "No Address Specified";
+    String paymentTotal = "No Address Specified";
+    String paymentType = "No Address Specified";
+    String purchaseDate = "No Address Specified";
+
+    
+    public Invoice(String custAddress, String fees, String custID, String discountTotal, String paymentTotal, String paymentType, String purchaseDate){
+    // Assigning each argument to the corresponding data field
+     this.custAddress = custAddress;
+     this.fees = fees;
+     this.custID = custID;
+     this.discountTotal = discountTotal;
+     this.paymentTotal = paymentTotal;
+     this.paymentType = paymentType;
+     this.purchaseDate = purchaseDate;
+    }
+
+
+    public String getCustAddress() {
+        return this.custAddress;
+    }
+    
+    public String getFees() {
+        return this.fees;
+    }
+    
+    public String getCustID() {
+        return this.custID;
+    }
+    
+    public String getDiscountTotal() {
+     return this.discountTotal;
+    }
+    
+    public String getPaymentTotal() {
+        return this.paymentTotal;
+    }
+    
+    public String getPaymentType () {
+        return this.paymentType;
+    }
+    
+    public String getPurchaseDate () {
+        return this.purchaseDate;
+    }
     
 }
--- src/main/java/org/dealership/controllerClasses/CreateInvoiceController.java
+++ src/main/java/org/dealership/controllerClasses/CreateInvoiceController.java
@@ -1,5 +1,5 @@
 /*
- * Author:       Chandler Ward
+ * Author:       Chandler Ward, Zhania Douglas
  * Written:      2 / 13 / 2024
  * Last Updated: 2 / 13 / 2024
  * 
@@ -17,6 +17,8 @@ package org.dealership.controllerClasses;
 import java.io.IOException;
 
 import org.dealership.driverClass;
+import org.dealership.Entities.Invoice;
+import org.dealership.backend.InvoiceDAO;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -55,6 +57,26 @@ public class CreateInvoiceController {
     @FXML
     void GoBackToEmpOptions(ActionEvent event) throws IOException {
         driver.changeScene("EmployeeOptionsPanel.fxml");
+    }
+
+    // This method is called by user clicking submit button
+    @FXML
+		public void createInvoice() {
+    	// Pull information from TextFields
+      String addressText = BuyerAddressInvoice.getText();
+      String feesText = BuyerFeesInvoice.getText();
+      String customerIdText = CustomerIDInvoice.getText();
+      String discountTotalText = DiscountTotalInvoice.getText();
+      String paymentTotalText = PaymentTotalInvoice.getText();
+      String paymentTypeText = PaymentTypeInvoice.getText();
+      String purchaseDateText = PurchaseDateInvoice.getText();
+      
+      Invoice inv = new Invoice(addressText, feesText, customerIdText, discountTotalText, paymentTotalText, paymentTypeText, purchaseDateText);
+      
+      // Pass the inv object to the DAO class for saving
+    	InvoiceDAO dao = new InvoiceDAO();
+    	dao.saveInvoice(inv);
+     
     }
     
 }
